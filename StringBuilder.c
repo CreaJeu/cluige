6a7e3209e2ca5dca963cff8d04e6fe636c4d1334
c-illy <azertyuiop>
2022-11-08 12:57:01 +0100

    regression fixed

--- StringBuilder.c
+++ StringBuilder.c
@@ -12,14 +12,14 @@
 static wchar_t* sbr_stringAlloc(StringBuilder* sb, size_t maxSize)
 {
     sb->nextChar = iCluige.checkedMalloc((maxSize + 1) * sizeof(wchar_t));
-    //sb->remainingSize = maxSize;
+    sb->remainingSize = maxSize;
     return sb->nextChar;
 }
 
 static void sbr_connectExistingString(StringBuilder* sb, wchar_t* dest)
 {
     sb->nextChar = dest;
-    //sb->remainingSize = wcslen(dest);
+    sb->remainingSize = wcslen(dest);
 }
 
 static void sbr_append(StringBuilder* sb, const wchar_t* formattedTail, ...)
@@ -27,9 +27,9 @@ static void sbr_append(StringBuilder* sb, const wchar_t* formattedTail, ...)
     va_list args;
     va_start(args, formattedTail);
 
-    //not C11 //int written = vswprintf(sb->nextChar, sb->remainingSize, formattedTail, args);
-    int written = vswprintf(sb->nextChar, formattedTail, args);
-    //sb->remainingSize -= written;
+    int written = vswprintf(sb->nextChar, sb->remainingSize, formattedTail, args);
+    //not C11 //int written = vswprintf(sb->nextChar, formattedTail, args);
+    sb->remainingSize -= written;
     sb->nextChar += written;
 
     va_end(args);
